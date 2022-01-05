04173633f04e1f003ba6d946806e7a3714e5beed
maxwell-miller-geo <mmm683@nau.edu>
2022-01-04 20:09:20 -0700

    Update Grade_Distribution.py
    Added references

--- Grade_Distribution.py
+++ Grade_Distribution.py
@@ -14,7 +14,7 @@ import statistics as stat
 def read_table(html):
     Time = html[23:-5].replace("_"," ") # slices string to find semester
     class_table = pd.read_html(html, skiprows = [1]) # skips blank row
-    under = class_table[0] 
+    under = class_table[0]
     cut = under.drop(0)
     data = cut.drop(axis = 1, columns = 0)
     col_length = data.shape[1] # number of columns
@@ -23,15 +23,15 @@ def read_table(html):
     df.index = [under.loc[y,0] for y in range(1,row_length+1)]
     df.columns = [under.loc[0,x] for x in range(1,col_length+1)]
     df['Instructor Name'] = df['Instructor Name'].astype(str)
-    
+
     # Add raw data into a new column: list method
     classes = df.index
     conv = {4: 'A',3:'B',2:'C',1:'D',0:'F'}
 
     df['Raw'] = None # Create empty raw data series
     df['Average'] = None # creates average column
-    df['Median'] = None 
-    df['Mode'] = None 
+    df['Median'] = None
+    df['Mode'] = None
     df['Standard Deviation'] = None
     df['Classes'] = df.index
 
@@ -42,12 +42,13 @@ def read_table(html):
             grade = 4 - x
             for y in range(number):
                     raw.append(grade)
-        df.iloc[row, 16] = pd.Series([raw]) # needs to be a series and enclosed with brackets
+        # Code below is supposed to insert the series of grades into 1 cell, but it does not work after saving it to another place WTF
+        #df.iloc[row, 16] = pd.Series([raw]) # needs to be a series and enclosed with brackets
         df.iloc[row, 17] = round(stat.mean(raw),3)
         df.iloc[row, 18] = stat.median(raw)
         df.iloc[row, 19] = stat.mode(raw)
         df.iloc[row, 20] = stat.stdev(raw)
-    
+
     plt.bar(df.index,df['Average'])
     plt.title('Mean GPA {}'.format(Time))
     plt.xlabel('Class')
@@ -56,15 +57,46 @@ def read_table(html):
     #plt.show()
     #plt.close()
     return df
-    
-# Read this 
-Fall2013 = read_table('C:/Users/mmmds/Desktop/GLGData/Fall_2003.html')
-Spring2014 = read_table('C:/Users/mmmds/Desktop/GLGData/Spring_2004.html')
 
+# Read this
+Fall_2003 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2003.html')
+Spring_2004 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2004.html')
+Fall_2004 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2004.html')
+Spring_2005 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2005.html')
+Fall_2005 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2005.html')
+Spring_2006 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2006.html')
+Fall_2006 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2006.html')
+Spring_2007 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2007.html')
+Fall_2007 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2007.html')
+Spring_2008 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2008.html')
+Fall_2008 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2008.html')
+Spring_2009 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2009.html')
+Fall_2009 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2009.html')
+Spring_2010 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2010.html')
+Fall_2010 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2010.html')
+Spring_2011 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2011.html')
+Fall_2011 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2011.html')
+Spring_2012 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2012.html')
+Fall_2012 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2012.html')
+Spring_2013 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2013.html')
+Fall_2013 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2013.html')
+Spring_2014 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2014.html')
+Fall_2014 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2014.html')
+Spring_2015 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2015.html')
+Fall_2015 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2015.html')
+Spring_2016 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2016.html')
+Fall_2016 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2016.html')
+Spring_2017 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2017.html')
+Fall_2017 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2017.html')
+Spring_2018 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2018.html')
+Fall_2018 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2018.html')
+Spring_2019 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2019.html')
+Fall_2019 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2019.html')
+Spring_2020 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2020.html')
+Fall_2020 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2020.html')
+Spring_2021 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2021.html')
 # Combine the datasets
-CY = pd.concat([Fall2013,Spring2014])
+CY = pd.concat([Fall_2003,Spring_2004])
 
 # Find Classes with certain Professors
 Wittke = CY[CY['Instructor Name'].str.contains('Wittke')]
-
-
