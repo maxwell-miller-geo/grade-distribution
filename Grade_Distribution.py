3958e094403ece860c14e26ed755f426d1afd0f5
maxwell-miller-geo <mmm683@nau.edu>
2022-01-05 20:30:54 -0700

    Update Grade_Distribution.py
    Started GUI implementation and created one dataset

--- Grade_Distribution.py
+++ Grade_Distribution.py
@@ -8,11 +8,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from unicodedata import normalize
 import statistics as stat
+from IPython.display import display
+import PySimpleGUI as sg
+import wx
+
 
 ###############
 
+########
+## PySimpleGUI
+##layout = [[sg.Text("NAU Geology Grade Distribution")], [sg.Button("OK")]]
+##
+### Create the window
+##window = sg.Window("NAU Geology Grade Distribution", layout)
+##
+### Create an event loop
+##while True:
+##    event, values = window.read()
+##    # End program if user closes window or
+##    # presses the OK button
+##    if event == "OK" or event == sg.WIN_CLOSED:
+##        break
+##window.close()
+
 def read_table(html):
-    Time = html[23:-5].replace("_"," ") # slices string to find semester
+    Time = html[59:-5].replace("_"," ") # slices string to find semester (59 for GITHub, 23 for Laptop)
     class_table = pd.read_html(html, skiprows = [1]) # skips blank row
     under = class_table[0]
     cut = under.drop(0)
@@ -28,12 +48,13 @@ def read_table(html):
     classes = df.index
     conv = {4: 'A',3:'B',2:'C',1:'D',0:'F'}
 
-    df['Raw'] = None # Create empty raw data series
+    #df['Raw'] = None # Create empty raw data series
     df['Average'] = None # creates average column
     df['Median'] = None
     df['Mode'] = None
-    df['Standard Deviation'] = None
-    df['Classes'] = df.index
+    df['σ'] = None #standard deviation
+    df['Semester'] = Time
+    #df['Classes'] = df.index
 
     for row in range(len(df.index)): # loops through all of the rows
         raw = []
@@ -42,18 +63,19 @@ def read_table(html):
             grade = 4 - x
             for y in range(number):
                     raw.append(grade)
-        # Code below is supposed to insert the series of grades into 1 cell, but it does not work after saving it to another place WTF
-        #df.iloc[row, 16] = pd.Series([raw]) # needs to be a series and enclosed with brackets
-        df.iloc[row, 17] = round(stat.mean(raw),3)
-        df.iloc[row, 18] = stat.median(raw)
-        df.iloc[row, 19] = stat.mode(raw)
-        df.iloc[row, 20] = stat.stdev(raw)
-
-    plt.bar(df.index,df['Average'])
-    plt.title('Mean GPA {}'.format(Time))
-    plt.xlabel('Class')
-    plt.ylabel('Mean GPA (4 point scale)')
-    plt.xticks(rotation = 30)
+        if len(raw) > 1:
+            # Code below is supposed to insert the series of grades into 1 cell, but it does not work after saving it to another place WTF
+            #df.iloc[row, 16] = pd.Series([raw]) # needs to be a series and enclosed with brackets
+            df.iloc[row, 16] = round(stat.mean(raw),3)
+            df.iloc[row, 17] = stat.median(raw)
+            df.iloc[row, 18] = stat.mode(raw)
+            df.iloc[row, 19] = stat.stdev(raw)
+            # Plot here
+        #plt.bar(df.index,df['Average'])
+        #plt.title('Mean GPA {}'.format(Time))
+        #plt.xlabel('Class')
+        #plt.ylabel('Mean GPA (4 point scale)')
+        #plt.xticks(rotation = 30)
     #plt.show()
     #plt.close()
     return df
@@ -95,8 +117,55 @@ Fall_2019 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distributi
 Spring_2020 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2020.html')
 Fall_2020 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Fall_2020.html')
 Spring_2021 = read_table('file:///C:/Users/themi/Documents/GitHub/grade-distribution/Spring_2021.html')
+
+# Display settings changes
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 # Combine the datasets
-CY = pd.concat([Fall_2003,Spring_2004])
+CY = pd.concat([Fall_2003,Spring_2004,Fall_2004,Spring_2005,Fall_2005,Spring_2006,
+                Fall_2006,Spring_2007,Fall_2007,Spring_2008,Fall_2008,Spring_2009,
+                Fall_2009,Spring_2010,Fall_2010,Spring_2011,Fall_2011,Spring_2012,
+                Fall_2012,Spring_2013,Fall_2013,Spring_2014,Fall_2014,Spring_2015,
+                Fall_2015,Spring_2016,Fall_2016,Spring_2017,Fall_2017,Spring_2018,
+                Fall_2018,Spring_2019,Fall_2019,Spring_2020,Fall_2020,Spring_2021])
 
 # Find Classes with certain Professors
 Wittke = CY[CY['Instructor Name'].str.contains('Wittke')]
+
+# Introducing the GUI!
+
+class MyFrame(wx.Frame):
+    
+##    def ask(parent=None, message='', default_value=''):
+##        dlg = wx.TextEntryDialog(parent, message, defaultValue=default_value)
+##        dlg.ShowModal()
+##        result = dlg.GetValue()
+##        dlg.Destroy()
+##        return result
+      
+    def __init__(self):
+        super().__init__(parent=None, title='NAU Grade Distribution')
+        panel = wx.Panel(self)        
+        my_sizer = wx.BoxSizer(wx.VERTICAL)        
+        self.text_ctrl = wx.TextCtrl(panel)
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+        my_btn = wx.Button(panel, label='Search')
+        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(my_sizer)        
+        self.Show()
+        
+    def on_press(self, event):
+        value = self.text_ctrl.GetValue()
+        if not value:
+            print("You didn't enter anything!")
+        else:
+            print(CY[CY['Instructor Name'].str.contains(value)])
+            print(f'You typed: "{value}"')
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+    
